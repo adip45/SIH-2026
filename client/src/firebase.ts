57c71b8828b14fe5537b6654cdfd59bdbd4094aa
Aditya <adyspatil@gmail.com>
2026-09-01 01:07:50 +0530

    Fix Firebase analytics build error

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -24,8 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+getAnalytics(app);
 // Initialize Firebase Authentication
 export const auth = getAuth(app);
 
